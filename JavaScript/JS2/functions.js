ae668463fca6ad6dcebdbcdd00f7e4a13cd91599
Abhishek Pratap Singh <31668873+abhish1ss@users.noreply.github.com>
2024-01-24 12:19:48 +0530

    Commit 2 Day 2

--- JavaScript/JS2/functions.js
+++ JavaScript/JS2/functions.js
@@ -7,19 +7,38 @@
 * */
 
 // Function Declaration, Function Statement
-function greet(from, to) {
-    console.log(from + " says hi to " + to);
-}
-
-greet('uttam','abhishek');
-
-
-const sayHiExpression = function (from, to) {
-    console.log(from + " says hi to " + to);
-    console.log("I am an anonymous function");
-}
-
-sayHiExpression('uttam','abhishek');
+// function greet(from, to) {
+//     console.log(from + " says hi to " + to);
+// }
+//
+// greet('uttam','abhishek');
+//
+//
+// const sayHiExpression = function (from, to) {
+//     console.log(from + " says hi to " + to);
+//     console.log("I am an anonymous function");
+// }
+//
+// sayHiExpression('uttam','abhishek');
 
 //First Class Functions || First Class Citizens
 //Functions can be used as methods, arguments, return types
+
+// console.log(a);
+// var a = 10;
+
+var a= 10;
+var b=20;
+
+function add(a,b) {
+    return a+b;
+}
+
+var result = add(a,b);
+var r1 = add(result,20);
+console.log(result);
+console.log(r1);
+
+//JavaScript Engine works in 2 phases
+//Memory Creation Phase/Memory Allocation Phase
+//Code Execution Phase
