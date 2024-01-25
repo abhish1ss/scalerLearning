b3f82494d4297867a58fea8433f705704c2d1646
Abhishek Pratap Singh <31668873+abhish1ss@users.noreply.github.com>
2024-01-25 22:59:21 +0530

    Added Functional Programming to learning

--- JavaScript/JS3/map.js
+++ JavaScript/JS3/map.js
@@ -3,5 +3,24 @@ const arr = [1,2,3,4,5,6,7,8,9,10,11];
 const callback = (number) => {
     return number * number;
 }
-const square = arr.map(callback);
+const square = arr.map((number, index,array) => {
+    return number * number;
+});
 console.log(square);
+
+const transaction = [1000,300,500,200,100];
+const inrToUsd = transaction.map((amount) => {
+    return amount * 0.014;
+});
+
+// console.log(inrToUsd);
+
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+const even = arr.filter((number) => {
+    return number % 2 === 0;
+}).map((number) => {
+    return number * number;
+});
+
+console.log(even);
